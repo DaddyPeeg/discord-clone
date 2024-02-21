d73267463b223fa0a0077810a53cad4a3f5036b7
David <david.estrelloso.tribugenia@gmail.com>
2024-02-21 19:11:47 +0800

    updated database

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -2,6 +2,7 @@ import { InitialModal } from "@/components/modals/initial-modal";
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
 import { redirect } from "next/navigation";
+import { useEffect } from "react";
 
 const SetupPage = async () => {
   const profile = await initialProfile();
